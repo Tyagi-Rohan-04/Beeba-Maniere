aa6170aed341c98ef0183b32e8c972bb4a8b94b7
Rohan <tyagirohan798@gmail.com>
2026-01-14 17:44:15 +0530

    Completed user login and user register page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
-import {Toaster} from "sonner";
+import { Toaster } from "sonner";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
-    <BrowserRouter future={{v7_startTransition: true, v7_relativeSplatPath: true}}>
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
       <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
         </Route>
         <Route>{/* Admin Layout */}</Route>
       </Routes>
