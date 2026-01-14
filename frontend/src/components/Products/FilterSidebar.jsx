ca36a269befe044c9d95e2dda843737314f51623
Rohan <tyagirohan798@gmail.com>
2026-01-15 02:47:41 +0530

    Added filter sidebar and sort options button on collection page

---
+++ frontend/src/components/Products/FilterSidebar.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const FilterSidebar = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [filters, setFilters] = useState({
+    category: "",
+    gender: "",
+    color: "",
+    size: [],
+    material: [],
+    brand: [],
+    minPrice: 0,
+    maxPrice: 100,
+  });
+
+  const [priceRange, setPriceRange] = useState([0, 100]);
+
+  return <div>FilterSidebar</div>;
+};
+
+export default FilterSidebar;
