7fa6a7505b363ad46182cfe953b6999488b7b762
Rohan <tyagirohan798@gmail.com>
2026-01-21 14:55:45 +0530

    Admin homepage UI completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import OrderConfirmationPage from "./pages/OrderConfirmationPage";
 import OrderDetailsPage from "./pages/OrderDetailsPage";
 import MyOrdersPage from "./pages/MyOrdersPage";
 import AdminLayout from "./components/Admin/AdminLayout";
+import AdminHomePage from "./pages/AdminHomePage";
 
 const App = () => {
   return (
@@ -37,7 +38,7 @@ const App = () => {
           <Route path="my-orders" element={<MyOrdersPage />} />
         </Route>
         <Route path="/admin" element={<AdminLayout />}>
-          {/* Admin Layout */}
+          <Route index element={<AdminHomePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
