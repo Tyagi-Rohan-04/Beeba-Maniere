5a22447c54d44799d1723c5dc0d7fb98e1439b32
Rohan <tyagirohan798@gmail.com>
2026-01-27 21:35:35 +0530

    create product request completed successfully

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
-// Load environment variables first
-dotenv.config();
-
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -23,6 +22,7 @@ app.get("/", (req, res) => {
 
 // API Routes
 app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
