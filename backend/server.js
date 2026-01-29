c6f799d85356da90f61a2c5305938dd3d791a01d
Rohan <tyagirohan798@gmail.com>
2026-01-29 13:53:54 +0530

    User CRUD routing created and tested successfully

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const checkoutRoutes = require("./routes/checkoutRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
 const subscriberRoute = require("./routes/subscriberRoute");
+const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
 app.use(express.json());
@@ -33,6 +34,11 @@ app.use("/api/checkout", checkoutRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api", subscriberRoute);
+
+
+// Admin Routes
+app.use("/api/admin/users", adminRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
