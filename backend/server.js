540c1d117b4e9f7acd35adccd30fb022f4daf225
Rohan <tyagirohan798@gmail.com>
2026-01-29 14:40:16 +0530

    Admin order CRUD routing completed and tested successfully

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const uploadRoutes = require("./routes/uploadRoutes");
 const subscriberRoute = require("./routes/subscriberRoute");
 const adminRoutes = require("./routes/adminRoutes");
 const productAdminRoutes = require("./routes/productAdminRoutes");
+const adminOrderRoutes = require("./routes/adminOrderRoutes");
 
 const app = express();
 app.use(express.json());
@@ -39,6 +40,7 @@ app.use("/api", subscriberRoute);
 // Admin Routes
 app.use("/api/admin/users", adminRoutes);
 app.use("/api/admin/products", productAdminRoutes);
+app.use("/api/admin/orders", adminOrderRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
